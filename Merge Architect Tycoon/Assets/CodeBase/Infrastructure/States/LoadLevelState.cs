0e646c16e258f5a48355867ce184758ecaa98b90
keshiah <kirylkisel@gmail.com>
2024-02-20 14:58:43 +0200

    Delete Everything

--- Merge Architect Tycoon/Assets/CodeBase/Infrastructure/States/LoadLevelState.cs
+++ Merge Architect Tycoon/Assets/CodeBase/Infrastructure/States/LoadLevelState.cs
@@ -1,8 +1,6 @@
 ﻿using CodeBase.CompositionRoot;
 using CodeBase.Infrastructure.Factories;
 using CodeBase.Services.PlayerProgressService;
-using CodeBase.Services.SceneContextProvider;
-using CodeBase.UI.Factories;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
@@ -12,22 +10,14 @@ namespace CodeBase.Infrastructure.States
     {
         private IGameStateMachine _gameStateMachine;
         private readonly ISceneLoader _sceneLoader;
-        private readonly IGameFactory _gameFactory;
-        private readonly IUIFactory _uiFactory;
-        private readonly ISceneContextProvider _sceneContextProvider;
         private readonly IPlayerProgressService _playerProgressService;
 
         private string _sceneName;
 
 
-        public LoadLevelState(ISceneLoader sceneLoader, ISceneContextProvider
-                sceneContextProvider, IGameFactory gameFactory, IUIFactory uiFactory,
-            IPlayerProgressService playerProgressService)
+        public LoadLevelState(ISceneLoader sceneLoader, PlayerProgressService playerProgressService)
         {
             _sceneLoader = sceneLoader;
-            _sceneContextProvider = sceneContextProvider;
-            _gameFactory = gameFactory;
-            _uiFactory = uiFactory;
             _playerProgressService = playerProgressService;
         }
 
@@ -35,8 +25,6 @@ namespace CodeBase.Infrastructure.States
         {
             _sceneName = sceneName;
 
-            _gameFactory.Cleanup();
-            _uiFactory.Cleanup();
 
             _sceneLoader.Load(sceneName, OnLoaded);
         }
@@ -52,28 +40,10 @@ namespace CodeBase.Infrastructure.States
 
         private async void OnLoaded()
         {
-            _sceneContextProvider.SetCurrentSceneContext(_sceneName);
-            _sceneContextProvider.Resolve<SceneObjectsProvider>().InitializeSceneObjects();
-
             await InitLevel();
-            await _gameFactory.WarmUp();
-            await _uiFactory.WarmUp();
         }
 
         private async UniTask InitLevel()
-        {
-            await CreateUi();
-            await CreatePools();
-        }
-
-        private async UniTask CreateUi()
-        {
-            await _uiFactory.CreateuiRoot();
-            await _uiFactory.CreateBuildingPopup();
-            await _uiFactory.CreateCoinsUi();
-        }
-
-        private async UniTask CreatePools()
         {
         }
     }
