e70b499778b8b1288b2b7bbcce35d1264c665bca
keshiah <kirylkisel@gmail.com>
2024-02-27 15:10:06 +0200

    fix merge

---
+++ Merge Architect Tycoon/Assets/CodeBase/Logic/Merge/MergePlane/MergePanelLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase._Gameplay.Merge.MergePlane
+{
+    public class MergePanelLoader : MonoBehaviour
+    {
+        public MergeGrid mergeGrid;
+
+        private void Start()
+        {
+            mergeGrid.InitializeGrid();
+        }
+    }
+}
