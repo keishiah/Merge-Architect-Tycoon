132180227fd3e3bcc7eb510bc8f93b3658bce659
keshiah <kirylkisel@gmail.com>
2024-03-04 15:09:47 +0200

    deleted cities

--- Merge Architect Tycoon/Assets/_Scripts/Logic/Progress.cs
+++ Merge Architect Tycoon/Assets/_Scripts/Logic/Progress.cs
@@ -9,14 +9,12 @@ namespace _Scripts.Logic
         public Coins Coins;
         public Coins Diamonds;
         public Buldings Buldings;
-        public Cities Cities;
 
         public Progress()
         {
             Coins = new Coins();
             Diamonds = new Coins();
             Buldings = new Buldings();
-            Cities = new Cities();
         }
 
         public void AddCoins(int coins)
@@ -35,25 +33,6 @@ namespace _Scripts.Logic
         {
             Buldings.AddCreatedBuildingToList(buildingName);
             SaveLoadService.Save(SaveKey.Progress, this);
-        }
-    }
-
-    public class Cities
-    {
-        private ReactiveDictionary<string, string> _createdCitiesDictionary = new();
-
-        public Cities()
-        {
-        }
-
-        public void AddCityToDictionary(string cityName)
-        {
-            _createdCitiesDictionary.Add(cityName, null);
-        }
-
-        public void AddDistrictToCity(string cityName, string districtName)
-        {
-            _createdCitiesDictionary.Add(cityName, districtName);
         }
     }
 }
